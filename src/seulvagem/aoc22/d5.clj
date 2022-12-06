47599a0efba82f508f413deddd9d39899c0a1fa8
Seulvagem <ffelipe.dias@gmail.com>
2022-12-05 23:09:32 -0300

    day 5 refactor (list version)

--- src/seulvagem/aoc22/d5.clj
+++ src/seulvagem/aoc22/d5.clj
@@ -6,45 +6,40 @@
             [clojure.set :as set]))
 
 (defn parse-crates-line [s]
-  ;; rest to skip the first [, mapping blank character to nil for readability and semantics
-  (->> s rest (take-nth 4) (map #(if (= % \ )
-                                       nil
-                                       %))))
+  (->> s rest (take-nth 4)))
 
-(defn get-rf-create-stacks [stack-ids]
-  (println stack-ids)
-  (let [initial-stacks (mapv (constantly '()) (conj stack-ids 0))]
-    (println initial-stacks)
+(defn get-rf-create-cargo [stack-ids]
+  (let [initial-stacks (reduce #(assoc %1 %2 '()) {} stack-ids)]
     (fn
       ([] initial-stacks)
-      ([stacks] stacks)
+      ([stacks] [stack-ids stacks])
       ([stacks [stack-id crate]]
        (update stacks stack-id conj crate)))))
 
-(defn parse-stack-ids [s]
-  (->> s (re-seq #"\d+" ) (map edn/read-string)))
+(defn digits-seq [s]
+  (re-seq #"\d+" s))
 
-(defn create-stacks [input-str]
+(defn create-cargo [input-str]
   (let [[ids-line & crates-lines] (-> input-str (str/split #"\n") rseq)
-        stack-ids (parse-stack-ids ids-line)
+        stack-ids                 (digits-seq ids-line)
+        stacks-count              (count stack-ids)
+        ix->id                    (zipmap (range) stack-ids)
 
         xf (comp (map parse-crates-line)
-                 (map #(map-indexed vector %))
-                 (map #(map (fn [[i c]] [(inc i) c]) %)) ;; inc index to match stack-id
-                 cat
+                 (mapcat #(map-indexed (fn [ix crate]
+                                         [(rem ix stacks-count)
+                                          (if (= crate \ )
+                                            nil
+                                            crate)]) %))
                  (filter #(get % 1))
-                 ) ;; flatten id,crate tuples
+                 (map #(update % 0 ix->id)))]
 
-        rf-create-stacks (get-rf-create-stacks stack-ids)
-        ]
-    (transduce xf rf-create-stacks crates-lines)))
+    (transduce xf (get-rf-create-cargo stack-ids) crates-lines)))
 
 (defn move-crate [stacks [from to]]
-  ;; (println "MOVE" from to)
   (let [from-stack (stacks from)
         to-stack (stacks to)
         crate (peek from-stack)]
-    (when-not crate (println "no crate bruh"))
     (-> stacks
         (update from pop)
         (update to conj crate))))
@@ -53,9 +48,6 @@
   (into '() (take qty stack)))
 
 (defn move-crates-9001 [stacks {:keys [qty from to]}]
-  ;; (println "MOVE" from to)
-  ;; (println :stacks stacks)
-  ;; (println :move-keys qty from to)
   (let [from-stack (stacks from)
         to-stack   (stacks to)
         crates     (take-and-reorder-crates qty from-stack)]
@@ -63,40 +55,38 @@
         (update from (partial drop qty))
         (update to (partial reduce conj) crates))))
 
-(defn step->moves [step-str]
-  (let [digits (re-seq #"\d+" step-str)
-        [qty & stack-ids] (map edn/read-string digits)
+(defn step->moves [[qty & stack-ids]]
+  (repeat (edn/read-string qty) (vec stack-ids)))
 
-        ]
+(defn step->move-map [[qty from to]]
+  {:qty (edn/read-string qty), :from from, :to to})
 
-    (repeat qty (vec stack-ids))
-    ))
-
-(defn step->move-map [step-str]
-  (let [digits            (re-seq #"\d+" step-str)
-        [qty from to] (map edn/read-string digits)]
-    {:qty qty, :from from, :to to}))
+(defn get-top-crates [stack-ids stacks]
+  (->>  stack-ids
+        (map (fn [id]
+               (-> id stacks first)))
+        (apply str)))
 
 (defn main [simple?]
   (let [input (u/get-input 5 simple?)
         [stacks-creation-input steps-input] (str/split input #"\R{2}")
-        initial-stacks (create-stacks stacks-creation-input)
-        steps (str/split-lines steps-input)
+        [stack-ids initial-stacks] (create-cargo stacks-creation-input)
+        step-strs (str/split-lines steps-input)
 
-        xf1 (comp (map step->moves)
-                 cat)
+        xf-parse-step-str (map digits-seq)
+
+        xf1 (comp xf-parse-step-str
+                  (mapcat step->moves))
         rf-move-crates (completing move-crate)
-        final-stacks1 (transduce xf1 rf-move-crates initial-stacks steps)
+        final-stacks1 (transduce xf1 rf-move-crates initial-stacks step-strs)
 
-        xf2 (comp (map step->move-map))
+        xf2 (comp xf-parse-step-str
+                  (map step->move-map))
         rf-move-crates-9001 (completing move-crates-9001)
-        final-stacks2 (transduce xf2 rf-move-crates-9001 initial-stacks steps)
+        final-stacks2 (transduce xf2 rf-move-crates-9001 initial-stacks step-strs)
 
-        get-top-crates #(->> % (do (pp/pprint %) %) (map first) (apply str))
-
-        res1 (get-top-crates final-stacks1)
-        res2 (get-top-crates final-stacks2)
-        ]
+        res1 (get-top-crates stack-ids final-stacks1)
+        res2 (get-top-crates stack-ids final-stacks2)]
     [res1 res2]))
 
 #_(main false)
